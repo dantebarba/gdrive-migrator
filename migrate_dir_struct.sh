dfc818f34b1b36264a62a4508efc73171626675b
Dante Barba <dantebarba@gmail.com>
2023-07-12 22:40:58 +0000

    Changed migration to local from cloud
    + Added shared volume support to allow using mergefs

--- migrate_dir_struct.sh
+++ migrate_dir_struct.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone copy old-gdrive: new-gdrive: --min-age 100y --create-empty-src-dirs --min-size 9999G --config rclone.conf --log-level DEBUG --drive-server-side-across-configs;
+rclone copy ${LOCAL_FS} gdrive: --min-age 100y --create-empty-src-dirs --min-size 9999G --config rclone.conf --log-level DEBUG --drive-server-side-across-configs;
