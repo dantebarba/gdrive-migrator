07949e6da22f09747649536cf20f2d4128650f05
Dante Barba <dantebarba@gmail.com>
2023-07-26 21:09:07 +0000

    added "-a" flag

--- start_migration.sh
+++ start_migration.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-mount /dev/sdd1 /mnt/disk1;
-mount /dev/sdc1 /mnt/disk2;
-mount /dev/sdf1 /mnt/disk3;
+mount -a /dev/sdd1 /mnt/disk1;
+mount -a /dev/sdc1 /mnt/disk2;
+mount -a /dev/sdf1 /mnt/disk3;
 
 docker-compose -f migration.yml up -d;
-docker logs -f gdrive-migrator;
+docker logs -f gdrive-migrato;r
