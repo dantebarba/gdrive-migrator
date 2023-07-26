41453be7e379f13b450081b2618f762986986b21
Dante Barba <dantebarba@gmail.com>
2023-07-26 21:58:24 +0000

    revert automount

--- start_migration.sh
+++ start_migration.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
 
-mount -a /dev/sdd1 /mnt/disk1;
-mount -a /dev/sdc1 /mnt/disk2;
-mount -a /dev/sdf1 /mnt/disk3;
-
 docker-compose -f migration.yml up -d;
 docker logs -f gdrive-migrator;
